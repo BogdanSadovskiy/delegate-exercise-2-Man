c38212743f14fed283b71dee36bfb55d40400787
Bogdan Sadovskiy <bohdansadovskiy@gmail.com>
2023-11-25 14:52:40 +0200

    Add project files.

---
+++ delegate exercise 2/Class1.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace Myspace
+{
+    internal class Men
+    {
+        public Men(string name, int age_)
+        {
+            Name = name;
+            age = age_;
+        }
+
+        public string Name { get; set; }
+        public int age { get; set; }
+
+        public override string? ToString()
+        {
+            return "Name: " + Name + "; Age: " + age.ToString();
+        }
+    }
+}
---
+++ delegate exercise 2/Program.cs
@@ -0,0 +1,34 @@
+﻿
+namespace Myspace;
+
+
+class program
+{
+    static void Main(string[] args)
+    {
+        List<Men> mens = new List<Men>();
+        mens.Add(new Men("Bogdan", 27));
+        mens.Add(new Men("Ostap", 13));
+        mens.Add(new Men("Bogdan Sad", 26));
+        mens.Add(new Men("Bandera", 160));
+        mens.Add(new Men("Non Name", 10000));
+        Console.WriteLine("\tAll man:");
+        mens.ForEach((men)=>Console.WriteLine(men));
+        Console.WriteLine("+-------------------------------+\n");
+
+        Console.WriteLine("\tSorted by names:");
+        mens.Sort((men1, men2) => men1.Name.CompareTo(men2.Name));
+        mens.ForEach((men) => Console.WriteLine(men));
+        Console.WriteLine("+-------------------------------+\n");
+
+        Console.WriteLine("\tMan older then 25:");
+        mens.RemoveAll((men) => men.age < 18);
+        mens.ForEach((men) => Console.WriteLine(men));
+        Console.WriteLine("+-------------------------------+\n");
+        mens.FindAll((men)=>men.age < 100).ForEach((men)=>Console.WriteLine(men));
+    }
+
+}
+
+
+
